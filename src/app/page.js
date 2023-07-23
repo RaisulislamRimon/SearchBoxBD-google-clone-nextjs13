4c10e74226d892c59ca6b530826f62a4f1e35b83
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-07-23 07:35:13 +0000

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,9 @@
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <h1 className="text-3xl font-bold text-red-600">
+        Hello world!
+      </h1>
+    </div>
   )
 }
