7eddcba0d6ae0e2a96043ac4258db5f800a715bf
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-07-23 07:48:30 +0000

    created header & footer components

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import HomeFooter from "@/components/HomeFooter";
+import HomeHeader from "@/components/HomeHeader";
+
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold text-red-600">
-        Hello world!
-      </h1>
+      <HomeHeader />
+      <HomeFooter />
     </div>
   )
 }
---
+++ src/components/HomeFooter.js
@@ -0,0 +1,5 @@
+export default function HomeFooter() {
+  return (
+    <div>HomeFooter</div>
+  )
+}
