0000815641e04c5386fc3a5e8089a3c5dc7d606f
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2023-07-25 18:55:12 +0600

    update the search box

--- src/components/HomeSearch.js
+++ src/components/HomeSearch.js
@@ -1,13 +1,24 @@
+"use client";
+
 import { AiOutlineSearch } from 'react-icons/ai';
 import { BsFillMicFill } from 'react-icons/bs';
+import {useState} from 'react'
 
 export default function HomeSearch() {
+    const [input, setInput] = useState("")
+    const handleSubmit = (e) => {
+    // function handleSubmit(e) {
+        e.preventDefault();
+        const form = e.target;
+        const searchInput = form.searchInput.value;
+        console.log(searchInput)
+    }
     return (
     <div>
-        <form className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl"
+        <form onSubmit={handleSubmit} className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl"
       >
             <AiOutlineSearch className='text-xl text-gray-500 mr-3' />
-            <input type="text" className="flex-grow focus:outline-none" />
+            <input type="text" name="searchInput" className="flex-grow focus:outline-none" />
             <BsFillMicFill className='text-xl text-gray-500 mr-3' />
         </form>
         <div className="flex flex-col space-y-2 sm:space-y-0 sm:space-x-4 justify-center items-center sm:flex-row mt-8">  
