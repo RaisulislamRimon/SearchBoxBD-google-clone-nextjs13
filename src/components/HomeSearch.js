2fa5e876d9a25456f8ec4fff4472df9445546d9f
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-07-25 13:27:16 +0000

    update

--- src/components/HomeSearch.js
+++ src/components/HomeSearch.js
@@ -2,29 +2,29 @@
 
 import { AiOutlineSearch } from 'react-icons/ai';
 import { BsFillMicFill } from 'react-icons/bs';
-import {useState} from 'react'
+import { useState } from 'react'
 
 export default function HomeSearch() {
-    const [input, setInput] = useState("")
-    const handleSubmit = (e) => {
+  const [input, setInput] = useState("")
+  const handleSubmit = (e) => {
     // function handleSubmit(e) {
-        e.preventDefault();
-        const form = e.target;
-        const searchInput = form.searchInput.value;
-        console.log(searchInput)
-    }
-    return (
+    e.preventDefault();
+    const form = e.target;
+    const searchInput = form.searchInput.value;
+    console.log(searchInput)
+  }
+  return (
     <div>
-        <form onSubmit={handleSubmit} className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl"
+      <form onSubmit={handleSubmit} className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl"
       >
-            <AiOutlineSearch className='text-xl text-gray-500 mr-3' />
-            <input type="text" name="searchInput" className="flex-grow focus:outline-none" />
-            <BsFillMicFill className='text-xl text-gray-500 mr-3' />
-        </form>
-        <div className="flex flex-col space-y-2 sm:space-y-0 sm:space-x-4 justify-center items-center sm:flex-row mt-8">  
-            <button className="btn">Google Search</button>
-            <button className="btn">I'm Feeling Lucky</button>
-        </div>
+        <AiOutlineSearch className='text-xl text-gray-500 mr-3' />
+        <input type="text" name="searchInput" className="flex-grow focus:outline-none" />
+        <BsFillMicFill className='text-xl text-gray-500 mr-3' />
+      </form>
+      <div className="flex flex-col space-y-2 sm:space-y-0 sm:space-x-4 justify-center items-center sm:flex-row mt-8">
+        <button className="btn">Google Search</button>
+        <button className="btn">I&apos;m Feeling Lucky</button>
+      </div>
     </div>
-    )
+  )
 }
