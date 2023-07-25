c4b76a0d188509c61b5da4824a22444abe2cb992
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2023-07-25 18:18:44 +0600

    added search_box and buttons

--- src/components/HomeSearch.js
+++ src/components/HomeSearch.js
@@ -2,18 +2,18 @@ import { AiOutlineSearch } from 'react-icons/ai';
 import { BsFillMicFill } from 'react-icons/bs';
 
 export default function HomeSearch() {
-  return (
+    return (
     <div>
-      <form className="flex w-full mt-5 mx-auto max-w-[90%] px-5 py-3 border border-gray-200 rounded-full ">
-        <AiOutlineSearch className='text-lg text-gray-500 mr-3' />
-        {/* <input type="text" className="w-full px-5 py-3 text-lg focus:outline-none border border-gray-200 rounded-full" /> */}
-        <input type="text" className="" />
-        <BsFillMicFill className='text-lg text-gray-500 mt-3' />
-        <div className="flex flex-row w-full space-y-2 justify-center mt-8 sm:flex-row sm:space-x-4 sm:space-y-0">
-          <button className="btn">Google Search</button>
-          <button className="btn">I&apos;m Feeling Lucky</button>
+        <form className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl"
+      >
+            <AiOutlineSearch className='text-xl text-gray-500 mr-3' />
+            <input type="text" className="flex-grow focus:outline-none" />
+            <BsFillMicFill className='text-xl text-gray-500 mr-3' />
+        </form>
+        <div className="flex flex-col space-y-2 sm:space-y-0 sm:space-x-4 justify-center items-center sm:flex-row mt-8">  
+            <button className="btn">Google Search</button>
+            <button className="btn">I'm Feeling Lucky</button>
         </div>
-      </form>
     </div>
-  )
+    )
 }
