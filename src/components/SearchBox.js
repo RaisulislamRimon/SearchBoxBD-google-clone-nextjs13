98b39905c3317aad3a2b63b60090caad71ec547d
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2023-07-29 01:09:27 +0600

    completed the search box component

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -23,7 +23,7 @@ export default function SearchBox() {
       <input type="text" className="w-full focus:outline-none" value={ term } onChange={ (e) => setTerm(e.target.value) } />
       <RxCross2 className="text-2xl text-gray-500 cursor-pointer sm:mr-2" onClick={ () => setTerm("") } />
       <BsFillMicFill className="hidden sm:inline-flex text-4xl text-blue-500 pl-4 border-l-2 border-gray-300 mr-3" />
-      <AiOutlineSearch className="text-2xl hidden sm:inline-flex text-blue-500 cursor-pointer" />
+      <AiOutlineSearch className="text-2xl hidden sm:inline-flex text-blue-500 cursor-pointer" onClick={ handleSubmit } />
     </form>
   );
 }
