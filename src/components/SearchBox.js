efdffa539e89e75560d6c4018b702cd5f3d62208
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2023-07-29 00:40:00 +0600

    update the search box

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,7 +1,22 @@
+"use client";
+
+import { RxCross2 } from "react-icons/rx";
+import { BsFillMicFill } from "react-icons/bs";
+import { AiOutlineSearch } from "react-icons/ai";
+import { useSearchParams } from "next/navigation";
+import { useState } from "react";
+
 export default function SearchBox() {
+  const searchParams = useSearchParams();
+  const searchTerm = searchParams.get("searchTerm");
+  const [term, setTerm] = useState(searchTerm || "");
+  console.log(searchTerm);
   return (
-    <div>
-      <h1>SearchBox</h1>
-    </div>
+    <form className="flex items-center border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl">
+      <input type="text" className="w-full focus:outline-none" defaultValue={ searchTerm } />
+      <RxCross2 className="text-2xl text-gray-500 cursor-pointer sm:mr-2" />
+      <BsFillMicFill className="hidden sm:inline-flex text-4xl text-blue-500 pl-4 border-l-2 border-gray-300 mr-3" />
+      <AiOutlineSearch className="text-2xl hidden sm:inline-flex text-blue-500 cursor-pointer" />
+    </form>
   );
 }
