9199e94b6fd963806ab008b43bbedc77b870be0c
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2023-07-30 11:45:43 +0600

    update icon

--- src/components/SearchHeaderOptions.js
+++ src/components/SearchHeaderOptions.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { AiOutlineSearch } from "react-icons/ai";
+import { AiOutlineSearch, AiOutlineCamera } from "react-icons/ai";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export default function SearchHeaderOptions() {
@@ -13,13 +13,13 @@ export default function SearchHeaderOptions() {
     router.push(`/search/${tabName === "Images" ? "image" : "web" }?searchTerm=${searchTerm}`)
   }
   return (
-    <div className="flex w-4/5 mx-auto">
+    <div className="flex space-x-2 border-b-4 mx-auto">
       <div onClick={ () => selectTab("All") } className={`flex items-center space-x-1 border-b-4 border-transparent active:text-blue-500 cursor-pointer pb-3 px-2 ${pathname === "/search/web" && "!text-blue-600 !border-blue-400"}`}>
         <AiOutlineSearch className="text-md" />
         <p>All</p>
       </div>
       <div onClick={ () => selectTab("Images") } className={`flex items-center space-x-1 border-b-4 border-transparent active:text-blue-500 cursor-pointer pb-3 px-2 ${pathname === "/search/image" && "!text-blue-600 !border-blue-400"}`}>
-        <AiOutlineSearch className="text-md" />
+        <AiOutlineCamera className="text-md" />
         <p>Images</p>
       </div>
     </div>
