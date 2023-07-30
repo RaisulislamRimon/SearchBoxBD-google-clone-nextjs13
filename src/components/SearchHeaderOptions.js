51987eac85e782cb86c186f1fda53303a1976ec1
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2023-07-30 10:08:20 +0600

    update

--- src/components/SearchHeaderOptions.js
+++ src/components/SearchHeaderOptions.js
@@ -1,16 +1,28 @@
 "use client";
 
 import { AiOutlineSearch } from "react-icons/ai";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export default function SearchHeaderOptions() {
   const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const searchTerm = searchParams.get("searchTerm");
+  
+  const selectTab = (tabName) => {
+    router.push(`/search/${tabName === "Images" ? "image" : "web" }?searchTerm=${searchTerm}`)
+  }
   return (
-    <div>
-      <div className={`flex items-center space-x-1 border-b-4 border-transparent active:text-blue-500 cursor-pointer pb-3 px-2 ${pathname === "/search/web" && "text-blue-600 border-blue-600"}`}>
+    <div className="flex w-4/5 mx-auto">
+      <div onClick={ () => selectTab("All") } className={`flex items-center space-x-1 border-b-4 border-transparent active:text-blue-500 cursor-pointer pb-3 px-2 ${pathname === "/search/web" && "!text-blue-600 !border-blue-400"}`}>
         <AiOutlineSearch className="text-md" />
         <p>All</p>
+      </div>
+      <div onClick={ () => selectTab("Images") } className={`flex items-center space-x-1 border-b-4 border-transparent active:text-blue-500 cursor-pointer pb-3 px-2 ${pathname === "/search/image" && "!text-blue-600 !border-blue-400"}`}>
+        <AiOutlineSearch className="text-md" />
+        <p>Images</p>
       </div>
     </div>
   );
 }
+
