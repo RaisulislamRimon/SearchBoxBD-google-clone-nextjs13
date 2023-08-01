1fcbb59cc104c08f17a74faf47adefabb60e002d
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2023-08-01 10:39:27 +0600

    update

--- src/components/WebSearchResults.js
+++ src/components/WebSearchResults.js
@@ -1,26 +1,22 @@
 import Link from "next/link";
 
-export default function WebSearchResults({ results }){
-  const { link, formattedUrl, title } = results;
+export default function WebSearchResults({ results }) {
+  // const { link, formattedUrl, title } = results;
   return (
     <div className="w-full mx-auto px-3 pb-24 sm:pl-[5%] md:pl-[14%] lg:pl-52">
       <p className="text-gray-600 text-sm mt-3 mb-5">
-        About {results?.searchInformation?.formattedTotalResults} results ({results?.searchInformation?.formattedSearchTime} seconds)
+        About {results?.searchInformation?.formattedTotalResults} results (
+        {results?.searchInformation?.formattedSearchTime} seconds)
       </p>
-      {
-        results?.items?.map(result => (
-          <div className="" key={link}>
-            <div className="">
-              <Link href={result?.link}>
-                { formattedUrl }                
-              </Link>
-              <Link href={result?.link}>
-                { title }                
-              </Link> 
-            </div>
+      {results?.items?.map((result) => (
+        <div className="" key={result?.link}>
+          <div className="">
+            <Link href={result?.link}>{result?.formattedUrl}</Link>
+            <Link href={result?.link}>{result?.title}</Link>
           </div>
-        ))
-      }
+          <p>{result?.htmlSnippet}</p>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
