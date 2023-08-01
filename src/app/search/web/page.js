146f9050f9d503cf1a54d36b9110dad0fb0694d7
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2023-08-01 21:15:23 +0600

    update

--- src/app/search/web/page.js
+++ src/app/search/web/page.js
@@ -2,7 +2,7 @@ import WebSearchResults from "@/components/WebSearchResults";
 
 export default async function WebSearchPage({ searchParams }) {
   const startIndex = searchParams.start || "1";
-  // await new Promise((resolve) => setTimeout(resolve, 10000));
+  await new Promise((resolve) => setTimeout(resolve, 5000));
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.GOOGLE_CONTEXT_API_KEY}&q=${searchParams?.searchTerm}&start=${startIndex}`
   );
