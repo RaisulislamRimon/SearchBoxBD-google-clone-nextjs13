6897a5b1cd14e319181243131d47c0b735160687
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2023-08-01 15:11:31 +0600

    showing pagination menu

--- src/components/ImageSearchResults.js
+++ src/components/ImageSearchResults.js
@@ -3,7 +3,7 @@ import PaginationButtons from "./PaginationButtons";
 
 export default function ImageSearchResults({ results }) {
   return (
-    <div className=" pb-48 sm:pb-24 mt-4 ">
+    <div className="pb-48 sm:pb-24 mt-4 ">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-3 space-x-4">
         {results?.items?.map((result) => (
           <div key={result?.link} className="mb-8">
