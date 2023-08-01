76267211b3b6820f93a13d101b79ac303042f739
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2023-08-01 11:35:41 +0600

    update

--- src/app/search/web/page.js
+++ src/app/search/web/page.js
@@ -2,41 +2,25 @@ import Link from "next/link";
 import WebSearchResults from "@/components/WebSearchResults";
 
 export default async function WebSearchPage({ searchParams }) {
-  const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.GOOGLE_CONTEXT_API_KEY}&q=${searchParams?.searchTerm}`)
-  
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.GOOGLE_CONTEXT_API_KEY}&q=${searchParams?.searchTerm}`
+  );
   if (!response?.ok) {
     throw new Error("Something went wrong...");
   }
-  
   const data = await response.json();
   const results = data?.items;
   const totalResults = data?.searchInformation?.totalResults;
-  // console.log(data?.searchInformation?.totalResults);
-  // console.log(results);
-  // console.log(searchParams?.searchTerm);
-  
+
   return (
     <>
-      {
-        totalResults === '0' && 
+      {totalResults === "0" && (
         <div className="text-2xl font-bold text-red-500 text-center pt-20">
-          <h1 className="">
-            No result found...
-          </h1>
+          <h1 className="">No result found...</h1>
         </div>
-      }
-      { 
-        // results && results.map(result => 
-        //   <div key={result?.title} className="border border-gray-300 p-4 m-4 rounded-lg">
-        //     <Link href={result?.link} target="_blank">
-        //       <p className="text-red-600">{result?.formattedUrl}</p>
-        //       <h1 className="text-2xl font-bold">{result?.title}</h1>
-        //       <p  className="text-xl">{result?.snippet}</p>
-        //     </Link>
-        //   </div>
-        // )
-        results && <WebSearchResults results={ data } />
-      }
+      )}
+      {results && <WebSearchResults results={data} />}
     </>
   );
 }
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -19,11 +19,25 @@ export default function SearchBox() {
     router.push(`/search/web?searchTerm=${term}`);
   };
   return (
-    <form onSubmit={ handleSubmit } className="flex items-center border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl">
-      <input type="text" className="w-full focus:outline-none" value={ term } onChange={ (e) => setTerm(e.target.value) } />
-      <RxCross2 className="text-2xl text-gray-500 cursor-pointer sm:mr-2" onClick={ () => setTerm("") } />
+    <form
+      onSubmit={handleSubmit}
+      className="flex items-center border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl"
+    >
+      <input
+        type="text"
+        className="w-full focus:outline-none"
+        value={term}
+        onChange={(e) => setTerm(e.target.value)}
+      />
+      <RxCross2
+        className="text-2xl text-gray-500 cursor-pointer sm:mr-2"
+        onClick={() => setTerm("")}
+      />
       <BsFillMicFill className="hidden sm:inline-flex text-4xl text-blue-500 pl-4 border-l-2 border-gray-300 mr-3" />
-      <AiOutlineSearch className="text-2xl hidden sm:inline-flex text-blue-500 cursor-pointer" onClick={ handleSubmit } />
+      <AiOutlineSearch
+        className="text-2xl hidden sm:inline-flex text-blue-500 cursor-pointer"
+        onClick={handleSubmit}
+      />
     </form>
   );
 }
