0bfe454aa1ba4240e6e177659648508b31a92ce8
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2023-08-01 18:47:25 +0600

    added pagination done

--- src/app/search/image/page.js
+++ src/app/search/image/page.js
@@ -1,9 +1,10 @@
 import ImageSearchResults from "@/components/ImageSearchResults";
 
 export default async function ImageSearchPage({ searchParams }) {
+  const startIndex = searchParams.start || "1";
   // await new Promise((resolve) => setTimeout(resolve, 10000));
   const response = await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.GOOGLE_CONTEXT_API_KEY}&q=${searchParams?.searchTerm}&searchType=image`
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.GOOGLE_CONTEXT_API_KEY}&q=${searchParams?.searchTerm}&searchType=image&start=${startIndex}`
   );
   if (!response?.ok) {
     throw new Error("Something went wrong...");
