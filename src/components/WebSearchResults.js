4ad0292f9103e95f34ab2624b3733aabd26a0acb
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2023-08-01 11:00:44 +0600

    showing result using html-react-parser npm package

--- src/components/WebSearchResults.js
+++ src/components/WebSearchResults.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
+import parse from "html-react-parser";
 
 export default function WebSearchResults({ results }) {
-  // const { link, formattedUrl, title } = results;
   return (
     <div className="w-full mx-auto px-3 pb-24 sm:pl-[5%] md:pl-[14%] lg:pl-52">
       <p className="text-gray-600 text-sm mt-3 mb-5">
@@ -14,7 +14,7 @@ export default function WebSearchResults({ results }) {
             <Link href={result?.link}>{result?.formattedUrl}</Link>
             <Link href={result?.link}>{result?.title}</Link>
           </div>
-          <p>{result?.htmlSnippet}</p>
+          <p>{parse(result?.htmlSnippet)}</p>
         </div>
       ))}
     </div>
